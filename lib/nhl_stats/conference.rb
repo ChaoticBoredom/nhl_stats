19ff16d9bdad3702f187ac2358392e61f368343d
Susan Wright <wright1@ualberta.ca>
2019-10-28 20:47:47 -0600

    Adding code to list and find of Conference

--- lib/nhl_stats/conference.rb
+++ lib/nhl_stats/conference.rb
@@ -4,5 +4,18 @@ module NHLStats
 
     def initialize(conference_data)
     end
+
+    def self.find(id)
+      response = Faraday.get("#{API_ROOT}/conferences/#{id}")
+      attributes = JSON.parse(response.body).dig("conferences", 0)
+      new(attributes)
+    end
+
+    def self.list
+      response = Faraday.get("#{API_ROOT}/conferences")
+      JSON.parse(response.body).
+        dig("conferences").
+        map { |c| NHLStats::Conference.new(c) }
+    end
   end
 end
