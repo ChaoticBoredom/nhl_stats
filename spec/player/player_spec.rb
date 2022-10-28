a401766b874dd9e42d77527899c36d3f1419f950
Susan Wright <wright1@ualberta.ca>
2022-10-28 16:00:00 -0600

    Add basic stats for players

--- spec/player/player_spec.rb
+++ spec/player/player_spec.rb
@@ -30,4 +30,38 @@ RSpec.describe NHLStats::Player do
       end
     end
   end
+
+  describe "#season_stats" do
+    it "should return an instance of PlayerStats" do
+      VCR.use_cassette("single_player_season_stats") do
+        player = NHLStats::Player.find(8478233)
+        expect(player.season_stats("20212022")).to be_instance_of(NHLStats::PlayerStats)
+      end
+    end
+
+    it "should return an instance of PlayerStats for the specified season" do
+      VCR.use_cassette("single_player_season_stats") do
+        player = NHLStats::Player.find(8478233)
+        season_stats = player.season_stats("20212022")
+        expect(season_stats.season).to eq "20212022"
+      end
+    end
+  end
+
+  describe "#current_season_stats" do
+    it "should return an instance of PlayerStats" do
+      VCR.use_cassette("current_season_stats") do
+        player = NHLStats::Player.find(8478233)
+        expect(player.current_season_stats).to be_instance_of(NHLStats::PlayerStats)
+      end
+    end
+
+    it "should return an instance of PlayerStats for the specified season" do
+      VCR.use_cassette("current_season_stats") do
+        player = NHLStats::Player.find(8478233)
+        season_stats = player.current_season_stats
+        expect(season_stats.season).to eq NHLStats.current_season_id
+      end
+    end
+  end
 end
