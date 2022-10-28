cd1c9cae56dcad201ebc311bb33b30da4955ebd8
Susan Wright <wright1@ualberta.ca>
2022-10-28 14:23:58 -0600

    Add method to get current_season_id and store it for duration of process

--- lib/nhl_stats.rb
+++ lib/nhl_stats.rb
@@ -4,6 +4,7 @@ require "nhl_stats/division"
 require "nhl_stats/franchise"
 require "nhl_stats/game"
 require "nhl_stats/player"
+require "nhl_stats/player_stats"
 require "nhl_stats/team"
 require "nhl_stats/schedule"
 require "nhl_stats/season"
@@ -16,4 +17,8 @@ API_ROOT = "https://statsapi.web.nhl.com/api/v1".freeze
 
 module NHLStats
   class Error < StandardError; end
+
+  def self.current_season_id
+    @current_season_id ||= NHLStats::Season.current.id
+  end
 end
